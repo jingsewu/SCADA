f358379e1d8954979af82b96c28f82be16030635
Evelyn <296807201@qq.com>
2025-04-15 14:48:23 +0800

    fix: update conveyor demo

--- client/src/pages/scada/monitor/ConnectedRectangles.tsx
+++ client/src/pages/scada/monitor/ConnectedRectangles.tsx
@@ -14,37 +14,36 @@ const ConnectedRectangles: React.FC = () => {
         const width = 1000
         const height = 800
         const margin = { top: 20, right: 20, bottom: 20, left: 20 }
-        const rectWidth = 40 // 绿色矩形宽度
-        const rectHeight = 60 // 绿色矩形高度
-        const verticalRectHeight = 40 // 绿色矩形高度
+        const rectWidth = 20 // 绿色矩形宽度
+        const rectHeight = 30 // 绿色矩形高度
         // Create SVG
         const svg = d3
             .select(svgRef.current)
-            .attr("width", width)
-            .attr("height", height)
+            .attr("width", "100%")
+            .attr("height", "100%")
 
         // Generate data points for the horizontal line
-        const numRectangles = 20 // 矩形个数
+        const numRectangles = 60 // 矩形个数
         const points = Array.from({ length: numRectangles }, (_, i) => ({
             x: rectWidth * (i + 1),
             y: rectHeight,
-            width: 40,
-            height: 60
+            width: rectWidth,
+            height: rectHeight
         }))
-        const verticalPoints = Array.from({ length: 10 }, (_, i) => ({
-            x: rectWidth * numRectangles + 120 - rectWidth / 2,
-            y: verticalRectHeight * i + rectHeight + 120,
+        const verticalPoints = Array.from({ length: 30 }, (_, i) => ({
+            x: rectWidth * numRectangles + 60 - rectWidth / 2,
+            y: rectWidth * i + rectHeight + 60,
             // y: 0,
-            width: 60,
-            height: 40
+            width: rectHeight,
+            height: rectWidth
         }))
         console.log("points", points)
 
         // Create arc generator
         const arc = d3
             .arc()
-            .innerRadius(60)
-            .outerRadius(120)
+            .innerRadius(30)
+            .outerRadius(60)
             .startAngle(0)
             .endAngle(Math.PI * 0.5) // 90 degrees
 
@@ -64,7 +63,10 @@ const ConnectedRectangles: React.FC = () => {
 
         // Add the arc path
         svg.append("path")
-            .attr("transform", `translate(${800}, ${rectHeight + 90})`)
+            .attr(
+                "transform",
+                `translate(${rectWidth * numRectangles}, ${rectHeight + 45})`
+            )
             .attr("d", arc as any)
             .attr("fill", "#b7eb8f")
             .attr("stroke", "#73d13d")
@@ -73,8 +75,8 @@ const ConnectedRectangles: React.FC = () => {
         // Create a group for moving boxes
         const boxGroup = svg.append("g")
         // Box parameters
-        const boxWidth = 40
-        const boxHeight = 60
+        const boxWidth = 20
+        const boxHeight = 30
         const boxColor = "#4F46E5"
         const boxStroke = "#3730A3"
 
