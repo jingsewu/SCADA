d676848993c095378f535b4b05b96c81d9b01ea1
Kinser <chymzyl@2751@gmail.com>
2026-04-11 14:45:45 +0800

    feat: use iframe to contains the conveyor scada.

--- client/build/webpack.config.example.dev.js
+++ client/build/webpack.config.example.dev.js
@@ -73,7 +73,13 @@ webpackConfig = {
             },
             {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                exclude: /node_modules/,
                 use: [{ loader: "@svgr/webpack", options: { icon: true } }]
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                include: /node_modules/,
+                use: ["file-loader"]
             }
         ]
     },
--- client/build/webpack.config.prod.js
+++ client/build/webpack.config.prod.js
@@ -53,7 +53,13 @@ module.exports = {
             },
             {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                exclude: /node_modules/,
                 use: [{ loader: "@svgr/webpack", options: { icon: true } }]
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                include: /node_modules/,
+                use: ["file-loader"]
             }
         ]
     },
