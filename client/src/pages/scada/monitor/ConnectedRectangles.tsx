d2c7b28b07db503b97235637d2c27598de7f7b2e
Kinser <chymzyl2751@gmail.com>
2026-04-12 18:07:00 +0800

    fix: remove 3d dependency.

--- client/src/pages/scada/monitor/ConnectedRectangles.tsx
+++ client/src/pages/scada/monitor/ConnectedRectangles.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const ConnectedRectangles: React.FC = () => {
     return (
         <iframe
-            src="http://117.50.245.4:8081/"
+            src="http://106.54.219.140:8081/"
             className="w-full h-full border-0"
             title="SCADA Conveyor Monitor"
             allowFullScreen
